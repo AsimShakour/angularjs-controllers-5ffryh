af2b52c317670435473f02b4092b5cb3ab710748
AsimShakour <shakourasim@gmail.com>
2019-12-23 10:48:37 -0600

    2 files modified http call working

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
-import angular from 'angular';
+import angular from "angular";
 
 // Create an angular module named 'app'.
-angular.module('app', []);
+angular.module("app", []);
 
 // Put application code here before bootstrap is called.
-angular.module('app').controller('MainCtrl', function ($scope,$http){
-  $http.get('data.json').then(function(response) {
-    console.log(response.data);
-    $scope.artists = response.data;
-  })
-});
+angular.module("app").controller("MainCtrl", function($scope, $http) {
+  console.log("this MainCntrl is being const")
+  $http.get("https://raw.githubusercontent.com/AsimShakour/angular/02_04b/builds/angular/js/data.json").then(function(response) 
+    { // First function handles success
+      console.log("We are successful in calling http get ");
+      console.log(response.data);
+      $scope.artists = response.data;
+    }, function(response){
+      // Second function handles error
+      console.log(response.status + " " +response.statusText)
+    }
+  )
+}
+)
+
 
 // Bootstrap angular onto the 'app' element, injecting the 'app' module.
-angular.bootstrap(document.getElementById('app'), ['app']);
+angular.bootstrap(document.getElementById("app"), ["app"]);
