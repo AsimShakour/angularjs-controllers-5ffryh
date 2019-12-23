596ce04030bd852ef035114149cc892f2a98b4f1
AsimShakour <shakourasim@gmail.com>
2019-12-23 09:40:21 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularjs-controllers-5ffryh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angularjs-controllers-5ffryh)
---
+++ app.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+
+// Create an angular module named 'app'.
+angular.module('app', []);
+
+// Put application code here before bootstrap is called.
+angular.module('app').controller('MainCtrl', function app($scope,$http){
+  $http.get('data.json').then(function(response) {
+  console.log = response.data;
+  $scope.artists = response.data;
+  })
+});
+
+// Bootstrap angular onto the 'app' element, injecting the 'app' module.
+angular.bootstrap(document.getElementById('app'), ['app']);
